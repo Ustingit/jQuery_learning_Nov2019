2bf6e1ce0650a48578b3d2e30eb47a5c614ea458
Ustin <ustinx@mail.ru>
2019-11-02 19:59:51 +0300

    first 2 lessons https://www.youtube.com/watch?v=pfsef_VWFR8&list=PLVfMKQXDAhGXQcouhIblV910Rv7lRscH3&index=2

--- 1/script.js
+++ 1/script.js
@@ -7,7 +7,7 @@ $('#btn-reset').click(function() {
 });
 
 //$('#content').css("color", "red");
-//$('article h2').css("color", "red");  //h2 in article == //$('article').find('h2').css("color", "red");
+//$('article h2').css("color", "red");  // h2 in article == //$('article').find('h2').css("color", "red");
 //$('h1 + p').css("color", "red"); //all p before which exist h1
 //$('#content').prev().css("color", "red");
 $('#content').next().css("color", "red");
