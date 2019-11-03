9972d887e13fa33af78c131db725f0c1655e9eb6
Ustin <ustinx@mail.ru>
2019-11-03 18:23:45 +0300

    13 lesson, fully works UP to button https://www.youtube.com/watch?v=d049Mr1WSFE&list=PLVfMKQXDAhGXQcouhIblV910Rv7lRscH3&index=13

--- 1/script.js
+++ 1/script.js
@@ -7,11 +7,20 @@ $('#btn-reset').click(function() {
 });
 
 
+// lesson 13 up to top button
 
+var $btnTop = $('.btn-top');
+$(window).on('scroll', function(){
+    if($(window).scrollTop() >= 20){
+        $btnTop.fadeIn();
+    } else {
+        $btnTop.fadeOut();
+    }
+});
 
-
-
-
+$btnTop.on('click', function(){
+    $('html,body').animate({scrollTop:0}, 900)
+});
 
 
 
